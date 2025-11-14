0c8e015b55b74d528b9249f709c73e65dc87e865
Max Dudkin <maxim.dudkin@genome.eu>
2025-11-14 20:40:58 +0200

    add stats plugin

--- init.lua
+++ init.lua
@@ -308,7 +308,20 @@ require('lazy').setup({
   --
   -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
   --
-
+  {
+    "gisketch/triforce.nvim",
+    dependencies = {
+      "nvzone/volt",
+    },
+    config = function()
+      require("triforce").setup({
+        -- Optional: Add your configuration here
+        keymap = {
+          show_profile = "<leader>tp", -- Open profile with <leader>tp
+        },
+      })
+    end,
+  },
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
   --    {
